fe92b8490c409f02b032d35f37888a236b0a951b
George Khalilieh <gkhalilieh@berkeley.edu>
2017-04-25 00:15:57 -0700

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,39 @@
-$(document).ready(function() ){};
+$(document).ready(function() {
 
-//  consider this http://unslider.com/
+$('#sidebar-button').click(function() {
+    if ($('#sidebar-button').hasClass('button-active')) {
+        $('body').removeClass('no-scroll');
+        $('.sidebar-container').removeClass('sidebar-active');
+        $('#sidebar-button').removeClass('button-active');
+        $('.page-wrapper').removeClass('wrapper-active');
+  }
+    else {
+        $('.sidebar-container').addClass('sidebar-active');
+        $('#sidebar-button').addClass('button-active');
+        $('.page-wrapper').addClass('wrapper-active');
+        setTimeout(function() {
+          $('body').addClass('no-scroll');}, 300);
+  }
+})
+
+$('.sidebar-item').click(function() {
+  $('body').removeClass('no-scroll');
+  $('.sidebar-container').removeClass('sidebar-active');
+  $('#sidebar-button').removeClass('button-active');
+  $('.page-wrapper').removeClass('wrapper-active');
+})
+
+$('.page-wrapper').click(function() {
+  if ($('#sidebar-button').hasClass('button-active')) {
+           $('body').removeClass('no-scroll');
+           $('.sidebar-container').removeClass('sidebar-active');
+           $('#sidebar-button').removeClass('button-active');
+           $('.page-wrapper').removeClass('wrapper-active');
+       }
+  })
+
+  $('.project-item').hover(function() {
+    $(this).css('opacity', .8);
+  })
+
+});
